60db6957f961aec7e5ee05581d5b3e11bc57fcb0
Iankj <indumia2@gmail.com>
2025-07-12 15:13:19 +0300

    Add simple_interest.py to calculate simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,11 +2,11 @@
 
 # Define values
 principal = 1000
-rate = 5  # percent
+rate = 0.05  # percent
 time = 3  # years
 
 # Calculate simple interest
-simple_interest = (principal * rate * time) / 100
+simple_interest = principal * rate * time
 
 # Print result
 print(f"Simple interest for principal {principal}, rate {rate}% and time {time} years is {simple_interest}")
