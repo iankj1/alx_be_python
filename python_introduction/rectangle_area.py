fff0edc13dad840e6d92782ad4d79a0f187ee3ad
Iankj <indumia2@gmail.com>
2025-07-12 15:22:23 +0300

    Find the area of the rectangle

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -3,7 +3,7 @@ length = 10
 width = 5
 
 #Area of the rectangle 
-Area_of_the_rectangle = length * width 
+Area_of_the_rectangle = width * length
 
 #dispaly the area
 print(f"Area of the rectangle is {Area_of_the_rectangle}")
