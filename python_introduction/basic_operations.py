b31296a3c44e1c8f2ec336ae8fdf134151fc40d5
Iankj <indumia2@gmail.com>
2025-07-12 15:07:50 +0300

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# initialization of the variables 
+number1 = 10
+number2 = 5
+
+#calculate operations
+sum_of_numbers = number1 + number2
+difference_in_numbers = number1 - number2
+product_of_numbers = number1  * number2
+
+#Display results 
+print(f"sum of {number1} and {number2} is sum_of_numbers")
+print(f"difference of {number1} and {number2} is difference_in_numbers")
+print(f"product of {number1} and {number2} is product_of_numbers")
